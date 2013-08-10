e13d392dfd4e5ea51b529bd557b58b099a116f53
root <root@raptorsrepublic.com>
2013-08-09 22:19:22 -0400

    Typo.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-# this is basically the basketball reference query string which is produced after you execute a searchy
+# this is basically the basketball reference query string which is produced after you execute a search
 
 url = 'http://www.basketball-reference.com/play-index/psl_finder.cgi?request=1&match=single&type=totals&per_minute_base=36&lg_id=NBA&is_playoffs=N&year_min=1980&year_max=&franch_id=&season_start=1&season_end=-1&age_min=0&age_max=99&height_min=0&height_max=99&birth_country_is=Y&birth_country=&is_active=&is_hof=&is_as=&as_comp=gt&as_val=0&pos_is_g=Y&pos_is_gf=Y&pos_is_f=Y&pos_is_fg=Y&pos_is_fc=Y&pos_is_c=Y&pos_is_cf=Y&qual=mp_per_g_req&c1stat=&c1comp=gt&c1val=&c2stat=&c2comp=gt&c2val=&c3stat=&c3comp=gt&c3val=&c4stat=&c4comp=gt&c4val=&c5stat=&c5comp=gt&c6mult=1.0&c6stat=&order_by=ws_per_48'
 
